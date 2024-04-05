27911d30d3167f45aecbee11c07edad0ad927c74
Jiatong Ding <jtding@bu.edu>
2024-04-05 01:35:42 -0400

    edit on clean and load for data

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -1,16 +1,44 @@
-
+library(data.table)
+library(magrittr)
+library(dplyr)
 library(tidyverse)
+
 ## CLEAN the data
-ds <- read_csv("dataset/NYPD.csv",show_col_types = FALSE )|>
+ds_2023 <- read_csv("dataset/NYPD.csv",show_col_types = FALSE )|>
   select(-ARREST_KEY,-PD_CD,-KY_CD,-LAW_CODE,-ARREST_BORO,-JURISDICTION_CODE) 
 
-na_count <- sum(is.na(ds))
+na_count <- sum(is.na(ds_2023))
 na_count
-ds <- na.omit(ds) 
-ds <- ds |>
+ds_2023<- na.omit(ds_2023) 
+ds_2023 <- ds_2023 |>
   na.omit() |>
   filter(PD_DESC != "(null)")|>
   filter(LAW_CAT_CD != "9")
+
+
+
+ds_2022 <-read_csv("dataset/NYPD_2022.csv",show_col_types = FALSE )|>
+  select(-ARREST_KEY,-PD_CD,-KY_CD,-LAW_CODE,-ARREST_BORO,-JURISDICTION_CODE) 
+na_count <- sum(is.na(ds_2022))
+na_count
+ds_2022 <- na.omit(ds_2022) 
+ds_2022 <- ds_2022 |>
+  na.omit() |>
+  filter(PD_DESC != "(null)")|>
+  filter(LAW_CAT_CD != "9")|>
+  filter(ARREST_DATE != "TRUE")
+
+ds_2022 %<>% dplyr::rename(`New Georeferenced Column` = Lon_Lat) # different column name
+
+ds <- rbind(ds_2023, ds_2022, use.names=TRUE)
+
+ds <- ds |>
+  na.omit() |>
+  filter(PD_DESC != "(null)")|>
+  filter(LAW_CAT_CD != "9")|>
+  filter(ARREST_DATE != "TRUE")
+
+fwrite(ds, 'NYPD_all.csv')
 
 saveRDS(ds, here::here("dataset","load_and_clean_data.rds"))
 
