076f959c5dd377b8972aa709cd01b427422fcbd9
Jiatong Ding <jtding@bu.edu>
2024-04-24 22:01:36 -0400

    edit on data set

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -3,38 +3,36 @@ library(magrittr)
 library(dplyr)
 library(tidyverse)
 
+
+
 ## CLEAN the data
-ds_2023 <- read_csv("dataset/NYPD.csv",show_col_types = FALSE )|>
+ds_2023 <- fread("dataset/NYPD.csv") |>
   select(-ARREST_KEY,-PD_CD,-KY_CD,-LAW_CODE,-JURISDICTION_CODE) 
 
 na_count <- sum(is.na(ds_2023))
 na_count
-ds_2023<- na.omit(ds_2023) 
+ds_2023 <- na.omit(ds_2023) 
 ds_2023 <- ds_2023 |>
   na.omit() |>
-  filter(PD_DESC != "(null)")|>
+  filter(PD_DESC != "(null)") |>
   filter(LAW_CAT_CD != "9")
 
-ds_2022 <-read_csv("dataset/NYPD_2022.csv",show_col_types = FALSE )|>
-  select(-ARREST_KEY,-PD_CD,-KY_CD,-LAW_CODE,-JURISDICTION_CODE) 
-na_count <- sum(is.na(ds_2022))
-na_count
-ds_2022 <- na.omit(ds_2022) 
-ds_2022 <- ds_2022 |>
-  na.omit() |>
-  filter(PD_DESC != "(null)")|>
-  filter(LAW_CAT_CD != "9")|>
-  filter(ARREST_DATE != "TRUE")
+ds_2023 %<>% mutate(ARREST_DATE = lubridate::mdy(ARREST_DATE))
 
-ds_2022 %<>% dplyr::rename(`New Georeferenced Column` = Lon_Lat) # different column name
 
-ds <- rbind(ds_2023, ds_2022, use.names=TRUE)
+ds_weather <- fread("dataset/weather.csv") %>% 
+  select(datetime, temp, windspeed, conditions)
+
+ds <- merge(ds_2023, ds_weather, by.x = 'ARREST_DATE', by.y = 'datetime', all.x = T)
+
+# fwrite(ds, 'ds_merge_data.csv')
 
 ds <- ds |>
   na.omit() |>
   filter(PD_DESC != "(null)")|>
-  filter(LAW_CAT_CD != "9")|>
-  filter(ARREST_DATE != "TRUE")
+  filter(LAW_CAT_CD != "9") |>
+  filter(ARREST_DATE != TRUE)
 
 saveRDS(ds, here::here("dataset","load_and_clean_data.rds"))
+
 
