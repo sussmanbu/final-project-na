6237ef7436524b9de5c4451800e0b701adf719c6
Jiatong Ding <jtding@bu.edu>
2024-04-29 14:06:27 -0400

    regression

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -1,29 +1,28 @@
-library(data.table)
-library(magrittr)
+
 library(dplyr)
+library(magrittr)
 library(tidyverse)
 
 
-
 ## CLEAN the data
-ds_2023 <- fread("dataset/NYPD.csv") |>
-  select(-ARREST_KEY,-PD_CD,-KY_CD,-LAW_CODE,-JURISDICTION_CODE,-X_COORD_CD,-Y_COORD_CD,
-         -Latitude,- Longitude) 
+ds_2023 <- fread("dataset/NYPD.csv") %>%
+  dplyr::select(-ARREST_KEY,-PD_CD,-KY_CD,-LAW_CODE,-JURISDICTION_CODE,-X_COORD_CD, -Y_COORD_CD, -Latitude, -Longitude) 
 
 na_count <- sum(is.na(ds_2023))
 na_count
+
+
 ds_2023 <- na.omit(ds_2023) 
 ds_2023 <- ds_2023 |>
   na.omit() |>
   filter(PD_DESC != "(null)") |>
-  filter(LAW_CAT_CD != "9")|>
-  filter(PERP_RACE != "UNKNOWN")
+  filter(LAW_CAT_CD != "9")
 
 ds_2023 %<>% mutate(ARREST_DATE = lubridate::mdy(ARREST_DATE))
 
 
 ds_weather <- fread("dataset/weather.csv") %>% 
-  select(datetime, temp, windspeed, conditions)
+  dplyr::select(datetime, temp, windspeed, conditions,cloudcover)
 
 ds <- merge(ds_2023, ds_weather, by.x = 'ARREST_DATE', by.y = 'datetime', all.x = T)
 
@@ -34,5 +33,7 @@ ds <- ds |>
   filter(ARREST_DATE != TRUE)
 
 saveRDS(ds, here::here("dataset","load_and_clean_data.rds"))
+
+
 
 
